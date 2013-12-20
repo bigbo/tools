b22e0a5ff41115f356c0962c22ea780dc22a90d1
ljingb <ljb90@live.cn>
2013-12-20 20:09:59 +0800

    update:add the new modus

--- python/Orz/MySQLClient.py
+++ python/Orz/MySQLClient.py
@@ -1,21 +1,34 @@
 import MySQLdb
 
 class MySQLdb(object):
-    def __init__(self,host,port,user,pawd,debug=False):
-        if
-        self.db = MySQLdb.connect(
-                host=host,
-                port=port,
-                user=user,
-                pawd=paws)
+    def __init__(self,host = 'localhost',user = None,pawd = 3306,debug=False):
+        try :
+            self.db = MySQLdb.connect(
+                                    host=host,
+                                    user=user,
+                                    pawd=pawd)
+            print "connect ok"
+        except:
+            print "connect error!"
 
-    def insert():
-        pass
+    def insert(self,str_sql):
+        cur_write = self.db.cursor()
+        try:
+            cur_write.execute(str_sql)
+            self.db.commit()
+        except Exception as e:
+            print("run Mysql save data: %s error: %s" % (sql, e))
 
-    def select(template,args):
-        pass
+    def select(self,str_sql):
+        conn = self.db.cursor()
+        try:
+            datas = conn.execute(str_sql)
+        except MySQLdb.Error,e:
+            print "Mysql error %d : %s." % (e.args[0], e.args[1])
+        return datas,conn
 
-    def close():
-        pass
+
+    def close(self,conn):
+        conn.close()
 
 
