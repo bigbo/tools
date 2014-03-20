4086f2c4db93e97de89fc50d249e2355a814d043
ljingb <ljb90@live.cn>
2014-03-20 15:27:17 +0800

    remove:empty file

--- python/Orz/json_example.py
+++
@@ -1,4 +0,0 @@
-import json
-
-print(json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}]))
-print()
--- python/Orz/test_class.py
+++
@@ -1,6 +0,0 @@
-class test_class :
-    def __init__(self):
-        print('init test class')
-
-    def print_info(self):
-        print('hello 4 test')
