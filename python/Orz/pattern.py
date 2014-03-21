c912f547f9d304e07e921b31f6e2d156d38f186d
ljingb <ljb90@live.cn>
2014-03-21 10:31:57 +0800

    remove:the empty file

--- python/Orz/pattern.py
+++
@@ -1,5 +0,0 @@
-class Factory(Object):
-    def __init__(self):
-        pass
-
-    def 
--- python/Orz/system.py
+++
@@ -1,9 +0,0 @@
-
-def cpu_number():
-    import multiprocessing 
-    try:
-        return multiprocessing.cpu_count()
-    except NotImplementedError:
-        return 1
-
-
